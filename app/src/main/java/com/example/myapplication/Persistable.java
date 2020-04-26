1c0d1626282326aae0264578f6e7f95ea04e82e3
abdul99j <abdul.dps@gmail.com>
2020-04-26 15:39:37 +0500

    Non-Exception Code

---
+++ app/src/main/java/com/example/myapplication/Persistable.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.content.SharedPreferences;
+import android.database.*;
+import android.database.sqlite.*;
+
+public interface Persistable{
+
+    void save(SQLiteDatabase dataStore);
+    void load(Cursor dataStore);
+    void delete(SQLiteDatabase dataStore);
+    String getId();
+    String getType();
+}
+
